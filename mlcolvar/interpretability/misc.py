11d7691bf52486bd432a55d45c073dab35d1f8fd
Pietro Novelli <pietronvll@gmail.com>
2023-06-23 13:33:11 +0100

    adding features

--- mlcolvar/interpretability/misc.py
+++ mlcolvar/interpretability/misc.py
@@ -129,10 +129,10 @@ def explain(descriptors: np.ndarray, labels: np.ndarray, feature_names: np.ndarr
         return explain_scalar(descriptors, labels, feature_names, options = options)
 
 def explain_scalar(descriptors: np.ndarray, labels: np.ndarray, feature_names: np.ndarray, options: ExplainerOptions = ExplainerOptions()):
-    model, _ = optimize_explainer_model(descriptors, labels, ir.fit_fn, ir.mape, feature_names, options = options, report_progress: bool = True)
+    model, _ = optimize_explainer_model(descriptors, labels, ir.fit_fn, ir.mape, feature_names, options = options, report_progress = True)
 
 def explain_boolean(descriptors: np.ndarray, labels: np.ndarray, feature_names: np.ndarray, options: ExplainerOptions = ExplainerOptions()):
-    model, _ = optimize_explainer_model(descriptors, labels, ic.fit_fn, ic.perc_error, feature_names, options = options, report_progress: bool = True)
+    model, _ = optimize_explainer_model(descriptors, labels, ic.fit_fn, ic.perc_error, feature_names, options = options, report_progress = True)
 
 def partition_categorical(descriptors: np.ndarray, labels: np.ndarray):
     pass
